08fb9752569fb4f00075d5c4fe669b9030a30d9c
Roaa <roaabbass@gmail.com>
2024-08-11 15:06:25 +0300

    task 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,9 +4,9 @@ import viteLogo from '/vite.svg';
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
 import './App.css';
-
 
 function App() {
   const [count, setCount] = useState(0);
@@ -41,6 +41,7 @@ function App() {
         <MainContent />
         <Footer />
       </div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   );
 }
