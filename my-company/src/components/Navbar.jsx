286092bb31d078f8ef1bd9182b4e199ccf4aa2b2
Roaa <roaabbass@gmail.com>
2024-08-16 23:14:49 +0300

    try

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
+
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#f8f8f8', marginBottom: '20px' }}>
+    <nav style={{ 
+      padding: '10px', 
+      borderBottom: '1px solid #ccc', 
+      display: 'flex',               
+      justifyContent: 'space-between' 
+    }}>
       <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
       <Link to="/about" style={{ marginRight: '10px' }}>About</Link>
       <Link to="/services" style={{ marginRight: '10px' }}>Services</Link>
-      <Link to="/contact" style={{ marginRight: '10px' }}>Contact</Link>
+      <Link to="/contact">Contact</Link>
     </nav>
   );
 }
