5aef4716fc55301d5dbaf8c01412b196e788cdb4
Roaa <roaabbass@gmail.com>
2024-08-16 22:58:21 +0300

    trying

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,14 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Services from './components/Services';
-import Contact from './components/Contact';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
+        <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+        <Link to="/about" style={{ marginRight: '10px' }}>About</Link>
+        <Link to="/services" style={{ marginRight: '10px' }}>Services</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
