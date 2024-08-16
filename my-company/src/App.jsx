ab8f648b8a51e4760a56417d75705dd8ed720214
Roaa <roaabbass@gmail.com>
2024-08-16 23:11:13 +0300

    task 3

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,18 +1,15 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Services from './Services';
 import Contact from './Contact';
+import Navbar from './Navbar'; 
 
 function App() {
   return (
     <Router>
-      <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
-        <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
-        <Link to="/about" style={{ marginRight: '10px' }}>About</Link>
-        <Link to="/services" style={{ marginRight: '10px' }}>Services</Link>
-        <Link to="/contact">Contact</Link>
-      </nav>
+      <Navbar /> {/* Include the Navbar component */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
