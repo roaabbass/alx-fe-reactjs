64ab8a5e4ebef2f98bec93ecefff3e33174f2ee4
Roaa <roaabbass@gmail.com>
2024-08-25 14:23:59 +0300

    task 1 - add the proper routes in the app.jsx hence its more originized

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
@@ -9,14 +9,12 @@ const App = () => {
   return (
     <Router>
       <div>
-        <h1>Recipe Sharing Application</h1>
-        <Switch>
-          <Route exact path="/" component={RecipeList} />
-          <Route path="/recipe/:id" render={(props) => (
-            <RecipeDetails recipeId={parseInt(props.match.params.id)} />
-          )} />
-        </Switch>
-        <AddRecipeForm />
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
       </div>
     </Router>
   );
