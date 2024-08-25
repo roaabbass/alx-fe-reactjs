30e89d136a3f83e98c6664b70be456bee0a21dd5
Roaa <roaabbass@gmail.com>
2024-08-25 14:13:01 +0300

    task 1

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,24 @@
+// src/App.jsx
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing Application</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing Application</h1>
+        <Switch>
+          <Route exact path="/" component={RecipeList} />
+          <Route path="/recipe/:id" render={(props) => (
+            <RecipeDetails recipeId={parseInt(props.match.params.id)} />
+          )} />
+        </Switch>
+        <AddRecipeForm />
+      </div>
+    </Router>
   );
 };
 
