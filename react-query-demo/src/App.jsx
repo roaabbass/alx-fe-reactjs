92180f076746af678f45db8ed4ce85679c3cf4af
Roaa <roaabbass@gmail.com>
2024-09-01 13:45:39 +0300

    .

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,7 +1,6 @@
 // src/App.jsx
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import PostsComponent from './PostsComponent';
 
 const queryClient = new QueryClient();
