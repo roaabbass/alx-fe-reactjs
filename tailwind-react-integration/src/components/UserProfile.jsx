7e609c0d3d282856f1ee99a8e307e88eb6807fb3
Roaa <roaabbass@gmail.com>
2024-09-06 13:38:43 +0300

    changes done in the code

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,7 +8,9 @@ function UserProfile() {
         alt="User" 
         className="rounded-full w-36 h-36 mx-auto" 
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className="text-xl text-blue-800 my-4">
+        John Doe
+      </h1>
       <p className="text-gray-600 text-base">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
