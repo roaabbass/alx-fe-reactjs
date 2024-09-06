bc737f5d8d271a0cb342f2a346e762cfc73fa11b
Roaa <roaabbass@gmail.com>
2024-09-06 13:23:51 +0300

    task -0 - replace the contect in js code to purge

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,7 +1,7 @@
 /**@type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: 'media',
   theme: { extend: {} },
   variants: { extend: {} },
