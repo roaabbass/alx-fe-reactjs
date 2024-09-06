95866c1f4af4dcc01b9c69e6e67fe20a42a5cec3
Roaa <roaabbass@gmail.com>
2024-09-06 21:25:24 +0300

    .

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
-        className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto" 
+        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto" 
       />
       <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4">
         John Doe
