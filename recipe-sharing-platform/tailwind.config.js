c0ee2e66c31877d1f6b3c9f4b8ce0c50b0918508
Roaa <roaabbass@gmail.com>
2024-09-09 14:31:52 +0300

    task 1 trial 4

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], 
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, 
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
