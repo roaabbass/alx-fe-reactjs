0239ff141207c673f8057ce1dcb151b98bbc3949
Roaa <roaabbass@gmail.com>
2024-09-11 10:36:14 +0300

    trying to fix the issue

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,8 +2,29 @@ import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  favorites: [],
+  recommendations: [],
+
+  addRecipe: (newRecipe) => set((state) => ({
+    recipes: [...state.recipes, newRecipe]
+  })),
+
   setRecipes: (recipes) => set({ recipes }),
+
+  addFavorite: (recipeId) => set((state) => ({
+    favorites: [...state.favorites, recipeId]
+  })),
+
+  removeFavorite: (recipeId) => set((state) => ({
+    favorites: state.favorites.filter((id) => id !== recipeId)
+  })),
+
+  generateRecommendations: () => set((state) => {
+    const recommended = state.recipes.filter(
+      (recipe) => state.favorites.includes(recipe.id) && Math.random() > 0.5
+    );
+    return { recommendations: recommended };
+  }),
 }));
 
 export default useRecipeStore;
