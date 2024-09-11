900d6c594d70c8ca8dff10731c6dbafddc45dbdc
Roaa <roaabbass@gmail.com>
2024-09-11 10:41:22 +0300

    trying to fix the errors

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,20 +5,37 @@ const useRecipeStore = create((set) => ({
   favorites: [],
   recommendations: [],
 
+  // Add a new recipe
   addRecipe: (newRecipe) => set((state) => ({
     recipes: [...state.recipes, newRecipe]
   })),
 
+  // Set the entire recipes array
   setRecipes: (recipes) => set({ recipes }),
 
+  // Update an existing recipe
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    )
+  })),
+
+  // Delete a recipe by ID
+  deleteRecipe: (recipeId) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId)
+  })),
+
+  // Add a recipe to favorites
   addFavorite: (recipeId) => set((state) => ({
     favorites: [...state.favorites, recipeId]
   })),
 
+  // Remove a recipe from favorites
   removeFavorite: (recipeId) => set((state) => ({
     favorites: state.favorites.filter((id) => id !== recipeId)
   })),
 
+  // Generate recommendations based on favorites
   generateRecommendations: () => set((state) => {
     const recommended = state.recipes.filter(
       (recipe) => state.favorites.includes(recipe.id) && Math.random() > 0.5
