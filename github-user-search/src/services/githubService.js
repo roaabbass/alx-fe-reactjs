982d2a2dbd4b591295dc223b0932c3ccbdc02ff0
Roaa <roaabbass@gmail.com>
2024-09-22 18:40:29 +0300

    .

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
 
 export const fetchUserData = async (username, location = '', minRepos = '') => {
+  // Construct the query string for advanced search
   let query = `q=${username}`;
   if (location) query += `+location:${location}`;
   if (minRepos) query += `+repos:>=${minRepos}`;
 
+  // Use GitHub Search API to search users with the specified query
   const response = await axios.get(`https://api.github.com/search/users?${query}`);
-  
+
   if (response.data.items.length === 0) {
     throw new Error('User not found');
   }
 
-  // Return an array of users
+  // Fetch additional user details from the user URL
   const users = await Promise.all(
     response.data.items.map(async (user) => {
       const userDetails = await axios.get(user.url);
@@ -19,5 +21,5 @@ export const fetchUserData = async (username, location = '', minRepos = '') => {
     })
   );
 
-  return users; // Return all user details
+  return users; // Return an array of users with detailed information
 };
